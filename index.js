55a92096736341f936ca34ef74883727a31851d8
ricardogcolombo <ricardogcolombo@gmail.com>
2017-01-17 10:48:56 -0300

    [ADD] eslintrc,editor config. CheckGitStatus function

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
+const exec = require('child_process').exec;
+const assert = require('assert').ok;
 
-
-var checkGitFiles = function(dir){
-    
+var checkGitFiles = function(dir) {
+    assert(typeof dir ==='string');
+    var parseDir = dir.split(' ');
+    assert(parseDir.length === 1);
+    //FIX: dir should not contain any command.
+    exec("git status " + dir, function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+    });
 };
+
+checkGitFiles("./")
